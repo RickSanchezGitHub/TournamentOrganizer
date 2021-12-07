4aa2baac67e78c2817085660cbbcc30ae70dd697
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-08 02:15:27 +0300

    added stored procedure for UPDATE player result by round of tournament

---
+++ TournamentOrganizer.DB/StoredProcedures/ResultTournamentPlayer_SetPlayerResultInRoundOfTournament.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[ResultTournamentPlayer_SetPlayerResultInRoundOfTournament]
+	@newResult int,
+	@PlayerId int,
+	@TournamentId int,
+	@NumberRound int
+AS
+BEGIN
+	UPDATE [dbo].[ResultTournamentPlayer]
+	   SET [Result] = @newResult
+	WHERE [PlayerId] = @PlayerId AND [TournamentId] = @TournamentId AND [NumberRound] = @NumberRound
+END
