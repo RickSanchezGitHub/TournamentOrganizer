c91b3169005df3cc919ed703b327cd6964a312f8
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-08 01:57:10 +0300

    added stored procedure for INSERT

---
+++ TournamentOrganizer.DB/StoredProcedures/ResultTournamentPlayer_InsertNewValues.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[ResultTournamentPlayer_InsertNewValues]
+	@PlayerId int,
+	@Result int,
+	@NumberRound int,
+	@NumberGame int,
+	@TournamentId int
+
+AS
+BEGIN
+	INSERT INTO [dbo].[ResultTournamentPlayer]
+           ([PlayerId]
+           ,[Result]
+           ,[NumberRound]
+           ,[NumberGame]
+           ,[TournamentId])
+     VALUES
+           (@PlayerId,
+			@Result,
+			@NumberRound,
+			@NumberGame,
+			@TournamentId)
+END
