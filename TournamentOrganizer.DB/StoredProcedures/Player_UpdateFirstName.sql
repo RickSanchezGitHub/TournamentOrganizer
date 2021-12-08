4b2845403ff291811b91445ff630e6744874e72b
Olga <eov01@yandex.ru>
2021-12-08 18:07:00 +0300

    Added Procedures for update Player

---
+++ TournamentOrganizer.DB/StoredProcedures/Player_UpdateFirstName.sql
@@ -0,0 +1,10 @@
+﻿Create proc dbo.Player_UpdateFirstName
+	(@id int,
+	 @First_Name varchar(25))
+as
+begin
+	update	Player
+	set
+		FirstName = @First_Name
+	where Id = @id
+end
---
+++ TournamentOrganizer.DB/StoredProcedures/Player_UpdateNickName.sql
@@ -0,0 +1,10 @@
+﻿Create Proc dbo.Player_UpdateNickName
+	(@id int,
+	 @Nick_Name varchar)
+as
+begin
+	update	Player
+	set
+		 NickName = @Nick_Name
+	where Id = @id
+end
