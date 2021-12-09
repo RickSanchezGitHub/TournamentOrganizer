df52336671c49da13c4b529540fc0e878b13e522
Alla <suharevaalla88@gmail.com>
2021-12-10 01:32:58 +0300

    Addit DeleteByTournomentId

--- TournamentOrganizer.DB/StoredProcedures/TeamTournamentHistory_DeleteByTournomentId.sql
+++ TournamentOrganizer.DB/StoredProcedures/TeamTournamentHistory_DeleteByTournomentId.sql
@@ -1,1 +1,8 @@
-﻿
+﻿CREATE PROCEDURE [dbo].[TeamTournamentHistory_DeleteByTournomentId]
+    @TournamentId int
+AS
+BEGIN
+     Delete [dbo].[TeamTournamentHistory]
+	WHERE TournamentId = @TournamentId
+	
+END
