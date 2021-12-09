90295f1cac0eb887ac58a9186fe0f3ea652b01dd
Alla <suharevaalla88@gmail.com>
2021-12-09 21:18:14 +0300

    Added tables

---
+++ TournamentOrganizer.DB/Tables/PlayerTournamentsHistory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [PlayerTournamentsHistory] (
+	Id integer NOT NULL,
+	TournamentId integer NOT NULL,
+	PlayerId integer NOT NULL,
+  CONSTRAINT [PK_PLAYERTOURNAMENTSHISTORY] PRIMARY KEY CLUSTERED
+  (
+  [Id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF), 
+    CONSTRAINT [FK_PlayerTournamentsHistory_ToTable] FOREIGN KEY ([TournamentId]) REFERENCES [Tournament]([Id])
+
+)
---
+++ TournamentOrganizer.DB/Tables/TeamTournamentsHistory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [TeamTournamentsHistory] (
+	Id integer NOT NULL,
+	TournamentId integer NOT NULL,
+	TeamId integer NOT NULL,
+  CONSTRAINT [PK_TEAMTOURNAMENTSHISTORY] PRIMARY KEY CLUSTERED
+  (
+  [Id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF), 
+    CONSTRAINT [FK_TeamTournamentsHistory_ToTable] FOREIGN KEY ([TournamentId]) REFERENCES [Tournament]([id])
+
+)
