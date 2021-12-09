c2ff27febd597ed3575a970c7681b173b94ce5c1
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-09 19:35:45 +0300

    added DataLayer project, entity ResultTournamentPlayer and trial stored procedure

---
+++ TournamentOrganizer.DataLayer/Entities/ResultTournamentPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.DataLayer.Entities
+{
+    class ResultTournamentPlayer
+    {
+        public int Id {get; set;}
+        public int PlayerId { get; set; }
+        public int Result { get; set; }
+        public int NumberRound { get; set; }
+        public int NumberGame { get; set; }
+        public int TournamentId { get; set; }
+    }
+}
---
+++ TournamentOrganizer.DataLayer/Repositories/ResultTournamentPlayerRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentOrganizer.DataLayer.Entities;
+
+namespace TournamentOrganizer.DataLayer.Repositories
+{
+    class ResultTournamentPlayerRepository
+    {
+        private const string _connectionString = "Data Source=LAPTOP-7HPLQHLI/TEW_SQLEXPRESS;Database=TournamentOrganizer.DB;Trusted_Connection=True;";
+
+        public ResultTournamentPlayer GetResultTournamentPlayerByPlayerIdAndTournamentId( int PlayerId, int TournamentId)
+        {
+            const string procName = "dbo.ResultTournamentPlayer_SelectByPlayerIdAndTournamentId";
+
+            using var connection = new SqlConnection(_connectionString);
+
+            using var command = new SqlCommand(procName, connection);
+
+            command.Parameters.AddWithValue("@PlayerId", PlayerId);
+            command.Parameters.AddWithValue("@TiurnamentId", TournamentId);
+
+            var result = new ResultTournamentPlayer();
+
+            try
+            {
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    result = new ResultTournamentPlayer
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal(nameof(ResultTournamentPlayer.Id))),
+                        PlayerId = reader.GetInt32(reader.GetOrdinal(nameof(ResultTournamentPlayer.PlayerId))),
+                        Result = reader.GetInt32(reader.GetOrdinal(nameof(ResultTournamentPlayer.Result))),
+                        NumberRound = reader.GetInt32(reader.GetOrdinal(nameof(ResultTournamentPlayer.NumberRound))),
+                        NumberGame = reader.GetInt32(reader.GetOrdinal(nameof(ResultTournamentPlayer.NumberGame))),
+                        TournamentId = reader.GetInt32(reader.GetOrdinal(nameof(ResultTournamentPlayer.TournamentId)))
+                    };
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+        
+
+        
+    }
+}
