ca31d06cb25bd4837b0475a1aa770ed1d4ceaf18
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-12 01:03:52 +0300

    create first stoored procedure

---
+++ TournamentOrganaizer.DataLayer/Entities/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.DataLayer.Entities
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
---
+++ TournamentOrganaizer.DataLayer/Repositories/ResultTournamentPlayerRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentOrganaizer.DataLayer.Repositories;
+using TournamentOrganizer.DataLayer.Entities;
+using Dapper;
+using TournamentOrganaizer.DataLayer.Entities;
+using System.Data;
+
+namespace TournamentOrganizer.DataLayer.Repositories
+{
+    public class ResultTournamentPlayerRepository
+    {
+        string ConnectionString = @"Data Source=LAPTOP-7HPLQHLI\TEW_SQLEXPRESS;Initial Catalog=TournamentOrganizer.DB;Integrated Security=True;";
+
+        string selectSql = "SELECT " +
+            "rtp.[Id]," +
+            "rtp.[Result]," +
+            "rtp.[NumberRound]," +
+            "rtp.[NumberMatch]," +
+            "p.Id," +
+            "p.FirstName," +
+            "p.LastName," +
+            "p.NickName" +
+
+            "FROM [dbo].[ResultTournamentPlayer] rtp inner join dbo.[Player] p  " +
+            "ON rtp.PlayerId = p.Id" +
+            //"inner join dbo.[Tournament] t" +
+            //"ON rtp.TournamentId = t.Id" +
+            "WHERE rtp.PlayerId = 1";
+
+        public List<ResultTournamentPlayer> GetPlayerResultsInAllTournaments()
+        {
+            
+            using var sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
+            var result = sqlConnection.Query<ResultTournamentPlayer, Player, ResultTournamentPlayer>(
+                "dbo.ResultTournamentPlayer_SelectPlayerResultsInAllTournaments",
+                (resultTournamentPlayer, player) =>
+                {
+                    resultTournamentPlayer.Player = player;
+                    return resultTournamentPlayer;
+                },
+                commandType: CommandType.StoredProcedure,
+                splitOn: "Id"
+                ).ToList();
+            return result;
+        }
+    }
+}
