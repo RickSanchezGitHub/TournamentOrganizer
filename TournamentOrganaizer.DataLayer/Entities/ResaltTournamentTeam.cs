fd627f8ad77e0473314e0167afe3abfe8668f9c7
Alla <suharevaalla88@gmail.com>
2021-12-12 02:56:34 +0300

    Added  class ResaltTournamentTeam and class Team

---
+++ TournamentOrganaizer.DataLayer/Entities/ResaltTournamentTeam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.DataLayer.Entities
+{
+   public class ResaltTournamentTeam
+    {
+        public int Id { get; set; }
+        public Team Team { get; set; }
+        public int Result { get; set; }
+        public int NumberRound { get; set; }
+        public int NumberMatch { get; set; }
+        public int Tournament { get; set; }
+    }
+}
