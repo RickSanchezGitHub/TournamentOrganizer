aeef633649d1f91db862114a5d9c0bf4ae744ab5
Olga <eov01@yandex.ru>
2021-12-12 23:56:57 +0300

    Added class Team_Player and Team_PlayerRepository, create methods, create Storage Procedure SelectByTeamId

--- TournamentOrganaizer.DataLayer/Repositories/Team_PlayerRepository.cs
+++ TournamentOrganaizer.DataLayer/Repositories/Team_PlayerRepository.cs
@@ -1,13 +1,60 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TournamentOrganaizer.DataLayer.Repositories;
+using TournamentOrganizer.DataLayer.Entities;
 
 namespace TournamentOrganizer.DataLayer.Repositories
 {
     public class Team_PlayerRepository
     {
-        
+        private string ConnectionString = RepositoryHelpers.ConnectionString;
+
+        public int Insert(Team_Player team_player)
+        {
+            int result = 0;
+            var procName = "Team_Player_Insert";
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                int? Id = db.Query<int>(procName, new
+                {
+                    TeamId = team_player.TeamId,
+                    PlayerId = team_player.PlayerId
+                },
+                    commandType: CommandType.StoredProcedure).FirstOrDefault();
+                if (Id is null)
+                {
+                    return result;
+                }
+                else
+                {
+                    result = Id.Value;
+                }
+            }
+            return result;
+        }
+
+        public void Delete(int Teamid, int PlayerId)
+        {
+            const string procedureName = "Team_Player_Delete";
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                int? Id = db.Execute(procedureName, new { Teamid }, commandType: CommandType.StoredProcedure);
+            }
+        }       
+
+        public List<Team_Player> GetById(int TeamId)
+        {
+            const string procedureName = "Team_PlayerSelectByTeamId";
+            using (IDbConnection db = new SqlConnection(ConnectionString))
+            {
+                return db.Query<Team_Player>(procedureName, new { TeamId }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+        }     
     }
 }
