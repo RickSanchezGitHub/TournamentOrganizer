cd2d332a6bd558cf224eadddd5db1a7e40f63bb6
Olga <eov01@yandex.ru>
2021-12-12 23:36:24 +0300

    Create class Team and Team_Player

---
+++ TournamentOrganaizer.DataLayer/Repositories/Team_PlayerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.DataLayer.Repositories
+{
+    public class Team_PlayerRepository
+    {
+        
+    }
+}
