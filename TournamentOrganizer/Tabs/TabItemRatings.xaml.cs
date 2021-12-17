28ddc491cc60786a0bc80f6f2a1a9f3104a71b00
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-17 17:46:07 +0300

    added maquette TabItem and folder Windows for convenience of presentation

---
+++ TournamentOrganizer/Tabs/TabItemRatings.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TournamentOrganizer.UI.Tabs
+{
+    /// <summary>
+    /// Логика взаимодействия для TabItemRatings.xaml
+    /// </summary>
+    public partial class TabItemRatings : TabItem
+    {
+        public TabItemRatings()
+        {
+            InitializeComponent();
+        }
+    }
+}
