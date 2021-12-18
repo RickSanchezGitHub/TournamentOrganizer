d53b440cb53729ff660ed19e5975766a74333178
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-18 22:50:53 +0300

    I am trying to perform a task according to the MVVM pattern, but so far it is not very successful (T_T)

--- TournamentOrganizer/Tabs/TabItemRatings.xaml.cs
+++ TournamentOrganizer/Tabs/TabItemRatings.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TournamentOrganizer.UI.VeiwModels.EntitiesVM;
+using TournamentOrganizer.UI.VeiwModels.TabItemsVM;
 
 namespace TournamentOrganizer.UI.Tabs
 {
@@ -20,9 +22,43 @@ namespace TournamentOrganizer.UI.Tabs
     /// </summary>
     public partial class TabItemRatings : TabItem
     {
+        public TabItemRatingsViewModel ViewModel;
+        
         public TabItemRatings()
         {
             InitializeComponent();
+            ViewModel = new TabItemRatingsViewModel();
+            ViewModel.Games.Add(new GameViewModel { Name = "Poker" });
+            ViewModel.Games.Add(new GameViewModel { Name = "Durak" });
+            ViewModel.Games.Add(new GameViewModel { Name = "Monopoly" });
+            DataContext = ViewModel;
+            ComboBoxSelectGame.ItemsSource = ViewModel.Games;
+        }
+
+        private void ComboBoxSelectGame_Selected(object sender, RoutedEventArgs e)
+        {
+            StackPanelRatingViewManager.Visibility = Visibility.Visible;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = (RadioButton)sender;
+
+            if (radioButton.Name == "RadioButtonPlayers")
+            {
+                DataGridParticipants.Columns.Clear();
+                DataGridParticipants.Columns.Add(new DataGridTextColumn { Header = "Имя" });
+                DataGridParticipants.Columns.Add(new DataGridTextColumn { Header = "Фамилия" });
+                DataGridParticipants.Columns.Add(new DataGridTextColumn { Header = "Погоняло" });
+                DataGridParticipants.Columns.Add(new DataGridTextColumn { Header = "Результат" });
+                
+            }
+            else
+            {
+                DataGridParticipants.Columns.Clear();
+                DataGridParticipants.Columns.Add(new DataGridTextColumn { Header = "Название" });
+                DataGridParticipants.Columns.Add(new DataGridTextColumn { Header = "Результат" });
+            }
         }
     }
 }
