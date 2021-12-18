ed8e0aaf465088340639ca16039bd62ebf536044
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-18 22:49:01 +0300

    Added two more folders to the ViewModels folder: EntitiesVM and TabItems.

---
+++ TournamentOrganizer/VeiwModels/EntitiesVM/GameViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.UI.VeiwModels.EntitiesVM
+{
+    public class GameViewModel : INotifyPropertyChanged
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+
+        private string? _name;
+        public string? Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TournamentOrganizer/VeiwModels/TabItemsVM/TabItemRatingsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using TournamentOrganizer.UI.VeiwModels.EntitiesVM;
+
+namespace TournamentOrganizer.UI.VeiwModels.TabItemsVM
+{
+    public class TabItemRatingsViewModel : BaseViewModel
+    {
+        public TabItemRatingsViewModel()
+        {
+            Games = new ObservableCollection<GameViewModel>();
+            
+        }
+
+        private ObservableCollection<GameViewModel> _games;
+        public ObservableCollection<GameViewModel> Games
+        {
+            get { return _games; }
+            set
+            {
+                _games = value;
+                OnPropertyChanged(nameof(Games));
+            }
+        }
+
+        public DataGrid dataGrid;
+
+       
+
+    }
+}
