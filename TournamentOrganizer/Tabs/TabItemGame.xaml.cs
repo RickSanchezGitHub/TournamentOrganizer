ecd7e1cadbf4fff20199100a12abad6bafb2a022
UserJul <yuliya.selezneva.1999@gmail.com>
2021-12-19 23:48:51 +0300

    Add Events Delite/Edit/Add

--- TournamentOrganizer/Tabs/TabItemGame.xaml.cs
+++ TournamentOrganizer/Tabs/TabItemGame.xaml.cs
@@ -16,25 +16,74 @@ using TournamentOrganizer.UI.VeiwModels;
 
 namespace TournamentOrganizer.UI.Tabs
 {
-    /// <summary>
-    /// Логика взаимодействия для TabItemGame.xaml
-    /// </summary>
+    /// <summary> 
+    /// Логика взаимодействия для TabItemGame.xaml 
+    /// </summary> 
     public partial class TabItemGame : TabItem
     {
         public TabItemGameViewModel ViewModel;
-
+        private GameViewModel _currentEditGame;
         public TabItemGame()
         {
             InitializeComponent();
+            DataContext = ViewModel;
             ViewModel = new TabItemGameViewModel();
             ViewModel.Games.Add(new GameViewModel { Name = "WoW" });
-            ViewModel.Games.Add(new GameViewModel { Name = "Monopolia" });
+            ViewModel.Games.Add(new GameViewModel { Name = "Monopoly" });
+            ViewModel.Games.Add(new GameViewModel { Name = "Durak" });
             DataGridListGames.ItemsSource = ViewModel.Games;
         }
 
-        private void ButtonGameDelite_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void ButtonGameDelite_Click(object sender, RoutedEventArgs e)
         {
+            var button = (Button)sender;
+            var game = (GameViewModel)button.DataContext;
+            ViewModel.Games.Remove(game);
 
+        }
+
+        private void ButtonGameEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var game = (GameViewModel)button.DataContext;
+            _currentEditGame = game;
+            TextBoxAddGameName.Text = _currentEditGame.Name;
+
+            ButtonAddEdit.Content = "Сохранить";
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TextBoxAddGameName.Text))
+            {
+                MessageBox.Show("Введите название игры");
+                return;
+            }
+
+            if (ViewModel.Games.FirstOrDefault(item => item.Name == TextBoxAddGameName.Text) != null)
+            {
+                MessageBox.Show("Такая игра уже есть");
+                return;
+            }
+
+            if ((string)ButtonAddEdit.Content == "Добавить")
+            {
+                ViewModel.Games.Add(new GameViewModel { Name = TextBoxAddGameName.Text });
+            }
+            else if ((string)ButtonAddEdit.Content == "Сохранить")
+            {
+                ButtonAddEdit.Content = "Добавить";
+                _currentEditGame.Name = TextBoxAddGameName.Text;
+            }
+        }
+
+        private void TextBoxAddGameName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBoxAddGameName.Text.Length > 25)
+            {
+                MessageBox.Show("Длинное название");
+                return;
+            }
         }
     }
 }
