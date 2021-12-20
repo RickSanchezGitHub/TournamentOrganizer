576d81323567e67dfd0f90acda3e5b9b61a61940
Aufniy Tiger <golenkotoxa@gmail.com>
2021-12-21 02:17:23 +0300

    added AutoMapper and create classes ResultTournamentPlayerModel, PlayerModel, TournamentModel

---
+++ TournamentOrganizer.BusinessLayer/Configuration/AutoMapper.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentOrganaizer.DataLayer.Entities;
+
+namespace TournamentOrganizer.BusinessLayer.Configuration
+{
+    public static class AutoMapper
+    {
+
+        private static Mapper _instance;
+
+        public static Mapper GetInstance()
+        {
+            if (_instance == null)
+            {
+                InitCustomMapper();
+            }
+            return _instance;
+        }
+        public static void InitCustomMapper()
+        {
+            _instance = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Tournament, TournamentModel>();
+
+            }));
+        }
+    }
+}
+
---
+++ TournamentOrganizer.BusinessLayer/Models/ResultTournamentPlayerModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.BusinessLayer.Models
+{
+    public class ResultTournamentPlayerModel: INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public PlayerModel Player { get; set; }
+        public int Result { get; set; }
+        public int NumberRound { get; set; }
+        public int NumberMatch { get; set; }
+        public TournamentModel Tournament { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
