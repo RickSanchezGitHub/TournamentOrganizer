cfc77781a7f333f2adf3a114aa9e48adb6b0594f
Olga <eov01@yandex.ru>
2021-12-22 17:42:24 +0300

    fixed

--- TournamentOrganizer/Tabs/TabItemParticipants.xaml.cs
+++ TournamentOrganizer/Tabs/TabItemParticipants.xaml.cs
@@ -20,7 +20,7 @@ namespace TournamentOrganizer.UI.Tabs
 {
 
     public partial class TabItemParticipants : TabItem
-    {
+    { 
         public TabItemParticipants()
         {
             InitializeComponent();
