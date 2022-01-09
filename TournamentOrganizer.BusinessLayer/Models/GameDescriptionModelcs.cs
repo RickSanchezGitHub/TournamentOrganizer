212e3653495633b3e9540e74d43ba80112d098ab
UserJul <yuliya.selezneva.1999@gmail.com>
2022-01-10 01:55:50 +0300

    Add Decriptions

---
+++ TournamentOrganizer.BusinessLayer/Models/GameDescriptionModelcs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentOrganizer.BusinessLayer.Models
+{
+    public class GameDescriptionModel : INotifyPropertyChanged
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+
+        private string _textDescription;
+        public string TextDescription
+        {
+            get { return _textDescription; }
+            set
+            {
+                _textDescription = value;
+                OnPropertyChanged(nameof(TextDescription));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
