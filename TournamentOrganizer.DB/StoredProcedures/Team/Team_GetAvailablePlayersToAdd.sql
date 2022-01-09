61aa06dc42d80087f20c825d8f5fae4074b9e8f9
Olga <eov01@yandex.ru>
2022-01-09 15:41:05 +0300

    fixed bugs command DeletePlayerForTeam in tab

---
+++ TournamentOrganizer.DB/StoredProcedures/Team/Team_GetAvailablePlayersToAdd.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Team_GetAvailablePlayersToAdd]
+	@TeamId int
+AS
+BEGIN
+
+Select* from Player
+EXCEPT
+Select p.* from  dbo.[Team] t 
+left join dbo.[Team_Player] tp on tp.TeamId = t.Id 
+left join dbo.[Player] p on p.Id = tp.PlayerId
+where t.Id = @TeamId
+
+END
