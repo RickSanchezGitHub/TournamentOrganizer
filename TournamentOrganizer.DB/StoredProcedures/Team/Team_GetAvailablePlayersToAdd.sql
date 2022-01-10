beb772c4b6e78040adbcbeeed50fa4b1b1e5e099
Olga <eov01@yandex.ru>
2022-01-10 23:08:35 +0300

    fixed storage Procedure Team_GetAvailablePlayersToAdd

--- TournamentOrganizer.DB/StoredProcedures/Team/Team_GetAvailablePlayersToAdd.sql
+++ TournamentOrganizer.DB/StoredProcedures/Team/Team_GetAvailablePlayersToAdd.sql
@@ -3,9 +3,19 @@
 AS
 BEGIN
 
-Select* from Player
+Select FirstName,
+	   LastName,
+	   Name,
+	   Email,
+	   Birthday 
+	   from Player
 EXCEPT
-Select p.* from  dbo.[Team] t 
+Select p.FirstName,
+	   p.LastName,
+	   p.Name,
+	   p.Email,
+	   p.Birthday 
+	   from dbo.[Team] t 
 left join dbo.[Team_Player] tp on tp.TeamId = t.Id 
 left join dbo.[Player] p on p.Id = tp.PlayerId
 where t.Id = @TeamId
